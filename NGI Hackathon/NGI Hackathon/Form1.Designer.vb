0dc698c2b2e76a27f227f680a22fa7885415a486
smlodzik <45125403+smlodzik@users.noreply.github.com>
2018-11-17 15:58:25 -0500

    Commiting Website

---
+++ NGI Hackathon/NGI Hackathon/Form1.Designer.vb
@@ -0,0 +1,322 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.btnContact = New System.Windows.Forms.Button()
+        Me.btnGame = New System.Windows.Forms.Button()
+        Me.btnInfo = New System.Windows.Forms.Button()
+        Me.lblQuote = New System.Windows.Forms.Label()
+        Me.lblInfo = New System.Windows.Forms.Label()
+        Me.btnHome = New System.Windows.Forms.Button()
+        Me.pbSuggestion = New System.Windows.Forms.PictureBox()
+        Me.pbFinances = New System.Windows.Forms.PictureBox()
+        Me.pbBankBot = New System.Windows.Forms.PictureBox()
+        Me.pbBankBot1 = New System.Windows.Forms.PictureBox()
+        Me.pbInstagram = New System.Windows.Forms.PictureBox()
+        Me.pbTwitter = New System.Windows.Forms.PictureBox()
+        Me.pbYoutube = New System.Windows.Forms.PictureBox()
+        Me.pbLinkedin = New System.Windows.Forms.PictureBox()
+        Me.pbFacebook = New System.Windows.Forms.PictureBox()
+        Me.pbLogo2 = New System.Windows.Forms.PictureBox()
+        Me.pbLogo1 = New System.Windows.Forms.PictureBox()
+        Me.pbGame = New System.Windows.Forms.PictureBox()
+        CType(Me.pbSuggestion, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbFinances, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbBankBot, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbBankBot1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbInstagram, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbTwitter, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbYoutube, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbLinkedin, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbFacebook, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbLogo2, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbLogo1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.pbGame, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'btnContact
+        '
+        Me.btnContact.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnContact.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnContact.Font = New System.Drawing.Font("Garamond", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnContact.ForeColor = System.Drawing.Color.Yellow
+        Me.btnContact.Location = New System.Drawing.Point(1058, 145)
+        Me.btnContact.Name = "btnContact"
+        Me.btnContact.Size = New System.Drawing.Size(264, 155)
+        Me.btnContact.TabIndex = 2
+        Me.btnContact.Text = "Contact Us"
+        Me.btnContact.UseVisualStyleBackColor = False
+        '
+        'btnGame
+        '
+        Me.btnGame.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnGame.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnGame.Font = New System.Drawing.Font("Garamond", 24.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnGame.ForeColor = System.Drawing.Color.Yellow
+        Me.btnGame.Location = New System.Drawing.Point(764, 145)
+        Me.btnGame.Name = "btnGame"
+        Me.btnGame.Size = New System.Drawing.Size(264, 155)
+        Me.btnGame.TabIndex = 3
+        Me.btnGame.Text = "The Cost of Life"
+        Me.btnGame.UseVisualStyleBackColor = False
+        '
+        'btnInfo
+        '
+        Me.btnInfo.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnInfo.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnInfo.Font = New System.Drawing.Font("Garamond", 30.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnInfo.ForeColor = System.Drawing.Color.Yellow
+        Me.btnInfo.Location = New System.Drawing.Point(467, 145)
+        Me.btnInfo.Name = "btnInfo"
+        Me.btnInfo.Size = New System.Drawing.Size(264, 155)
+        Me.btnInfo.TabIndex = 4
+        Me.btnInfo.Text = "More Info"
+        Me.btnInfo.UseVisualStyleBackColor = False
+        '
+        'lblQuote
+        '
+        Me.lblQuote.AutoSize = True
+        Me.lblQuote.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblQuote.Font = New System.Drawing.Font("Segoe Script", 20.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblQuote.ForeColor = System.Drawing.Color.DarkGreen
+        Me.lblQuote.Location = New System.Drawing.Point(273, 45)
+        Me.lblQuote.Name = "lblQuote"
+        Me.lblQuote.Size = New System.Drawing.Size(923, 67)
+        Me.lblQuote.TabIndex = 5
+        Me.lblQuote.Text = "Get ready to take control of your finances!"
+        '
+        'lblInfo
+        '
+        Me.lblInfo.BackColor = System.Drawing.Color.LemonChiffon
+        Me.lblInfo.Font = New System.Drawing.Font("Papyrus", 20.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.lblInfo.Location = New System.Drawing.Point(61, 358)
+        Me.lblInfo.Name = "lblInfo"
+        Me.lblInfo.Size = New System.Drawing.Size(829, 643)
+        Me.lblInfo.TabIndex = 7
+        '
+        'btnHome
+        '
+        Me.btnHome.BackColor = System.Drawing.Color.RoyalBlue
+        Me.btnHome.FlatStyle = System.Windows.Forms.FlatStyle.Popup
+        Me.btnHome.Font = New System.Drawing.Font("Garamond", 36.0!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.btnHome.ForeColor = System.Drawing.Color.Yellow
+        Me.btnHome.Location = New System.Drawing.Point(168, 145)
+        Me.btnHome.Name = "btnHome"
+        Me.btnHome.Size = New System.Drawing.Size(264, 155)
+        Me.btnHome.TabIndex = 9
+        Me.btnHome.Text = "Home"
+        Me.btnHome.UseVisualStyleBackColor = False
+        '
+        'pbSuggestion
+        '
+        Me.pbSuggestion.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.suggestion_box
+        Me.pbSuggestion.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbSuggestion.Location = New System.Drawing.Point(788, 347)
+        Me.pbSuggestion.Name = "pbSuggestion"
+        Me.pbSuggestion.Size = New System.Drawing.Size(513, 607)
+        Me.pbSuggestion.TabIndex = 20
+        Me.pbSuggestion.TabStop = False
+        '
+        'pbFinances
+        '
+        Me.pbFinances.BackgroundImage = CType(resources.GetObject("pbFinances.BackgroundImage"), System.Drawing.Image)
+        Me.pbFinances.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbFinances.Location = New System.Drawing.Point(918, 377)
+        Me.pbFinances.Name = "pbFinances"
+        Me.pbFinances.Size = New System.Drawing.Size(497, 450)
+        Me.pbFinances.TabIndex = 19
+        Me.pbFinances.TabStop = False
+        '
+        'pbBankBot
+        '
+        Me.pbBankBot.BackgroundImage = CType(resources.GetObject("pbBankBot.BackgroundImage"), System.Drawing.Image)
+        Me.pbBankBot.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbBankBot.Location = New System.Drawing.Point(851, 690)
+        Me.pbBankBot.Name = "pbBankBot"
+        Me.pbBankBot.Size = New System.Drawing.Size(624, 137)
+        Me.pbBankBot.TabIndex = 18
+        Me.pbBankBot.TabStop = False
+        Me.pbBankBot.Visible = False
+        '
+        'pbBankBot1
+        '
+        Me.pbBankBot1.BackgroundImage = CType(resources.GetObject("pbBankBot1.BackgroundImage"), System.Drawing.Image)
+        Me.pbBankBot1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbBankBot1.Location = New System.Drawing.Point(933, 316)
+        Me.pbBankBot1.Name = "pbBankBot1"
+        Me.pbBankBot1.Size = New System.Drawing.Size(469, 425)
+        Me.pbBankBot1.TabIndex = 17
+        Me.pbBankBot1.TabStop = False
+        Me.pbBankBot1.Visible = False
+        '
+        'pbInstagram
+        '
+        Me.pbInstagram.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.instagram_logo
+        Me.pbInstagram.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbInstagram.Location = New System.Drawing.Point(227, 597)
+        Me.pbInstagram.Name = "pbInstagram"
+        Me.pbInstagram.Size = New System.Drawing.Size(146, 144)
+        Me.pbInstagram.TabIndex = 15
+        Me.pbInstagram.TabStop = False
+        Me.pbInstagram.Visible = False
+        '
+        'pbTwitter
+        '
+        Me.pbTwitter.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.twitter_logo
+        Me.pbTwitter.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbTwitter.Location = New System.Drawing.Point(397, 597)
+        Me.pbTwitter.Name = "pbTwitter"
+        Me.pbTwitter.Size = New System.Drawing.Size(146, 144)
+        Me.pbTwitter.TabIndex = 14
+        Me.pbTwitter.TabStop = False
+        Me.pbTwitter.Visible = False
+        '
+        'pbYoutube
+        '
+        Me.pbYoutube.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.youtube_logo
+        Me.pbYoutube.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbYoutube.Location = New System.Drawing.Point(138, 767)
+        Me.pbYoutube.Name = "pbYoutube"
+        Me.pbYoutube.Size = New System.Drawing.Size(146, 144)
+        Me.pbYoutube.TabIndex = 13
+        Me.pbYoutube.TabStop = False
+        Me.pbYoutube.Visible = False
+        '
+        'pbLinkedin
+        '
+        Me.pbLinkedin.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.linkedin_logo
+        Me.pbLinkedin.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbLinkedin.Location = New System.Drawing.Point(310, 767)
+        Me.pbLinkedin.Name = "pbLinkedin"
+        Me.pbLinkedin.Size = New System.Drawing.Size(146, 144)
+        Me.pbLinkedin.TabIndex = 12
+        Me.pbLinkedin.TabStop = False
+        Me.pbLinkedin.Visible = False
+        '
+        'pbFacebook
+        '
+        Me.pbFacebook.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.facebook_logo
+        Me.pbFacebook.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbFacebook.Location = New System.Drawing.Point(57, 597)
+        Me.pbFacebook.Name = "pbFacebook"
+        Me.pbFacebook.Size = New System.Drawing.Size(146, 144)
+        Me.pbFacebook.TabIndex = 11
+        Me.pbFacebook.TabStop = False
+        Me.pbFacebook.Visible = False
+        '
+        'pbLogo2
+        '
+        Me.pbLogo2.BackgroundImage = CType(resources.GetObject("pbLogo2.BackgroundImage"), System.Drawing.Image)
+        Me.pbLogo2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbLogo2.Location = New System.Drawing.Point(1329, -5)
+        Me.pbLogo2.Name = "pbLogo2"
+        Me.pbLogo2.Size = New System.Drawing.Size(172, 195)
+        Me.pbLogo2.TabIndex = 10
+        Me.pbLogo2.TabStop = False
+        '
+        'pbLogo1
+        '
+        Me.pbLogo1.BackgroundImage = CType(resources.GetObject("pbLogo1.BackgroundImage"), System.Drawing.Image)
+        Me.pbLogo1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbLogo1.Location = New System.Drawing.Point(-10, 16)
+        Me.pbLogo1.Name = "pbLogo1"
+        Me.pbLogo1.Size = New System.Drawing.Size(182, 174)
+        Me.pbLogo1.TabIndex = 8
+        Me.pbLogo1.TabStop = False
+        '
+        'pbGame
+        '
+        Me.pbGame.BackgroundImage = Global.NGI_Hackathon.My.Resources.Resources.game_screen
+        Me.pbGame.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch
+        Me.pbGame.Location = New System.Drawing.Point(918, 377)
+        Me.pbGame.Name = "pbGame"
+        Me.pbGame.Size = New System.Drawing.Size(497, 450)
+        Me.pbGame.TabIndex = 21
+        Me.pbGame.TabStop = False
+        Me.pbGame.Visible = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(9.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.Color.LemonChiffon
+        Me.ClientSize = New System.Drawing.Size(1497, 1050)
+        Me.Controls.Add(Me.pbGame)
+        Me.Controls.Add(Me.pbSuggestion)
+        Me.Controls.Add(Me.pbFinances)
+        Me.Controls.Add(Me.pbBankBot)
+        Me.Controls.Add(Me.pbBankBot1)
+        Me.Controls.Add(Me.pbInstagram)
+        Me.Controls.Add(Me.pbTwitter)
+        Me.Controls.Add(Me.pbYoutube)
+        Me.Controls.Add(Me.pbLinkedin)
+        Me.Controls.Add(Me.pbFacebook)
+        Me.Controls.Add(Me.pbLogo2)
+        Me.Controls.Add(Me.btnHome)
+        Me.Controls.Add(Me.pbLogo1)
+        Me.Controls.Add(Me.lblInfo)
+        Me.Controls.Add(Me.lblQuote)
+        Me.Controls.Add(Me.btnInfo)
+        Me.Controls.Add(Me.btnGame)
+        Me.Controls.Add(Me.btnContact)
+        Me.MaximumSize = New System.Drawing.Size(1519, 1106)
+        Me.MinimumSize = New System.Drawing.Size(1519, 1018)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.pbSuggestion, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbFinances, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbBankBot, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbBankBot1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbInstagram, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbTwitter, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbYoutube, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbLinkedin, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbFacebook, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbLogo2, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbLogo1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.pbGame, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+    Friend WithEvents btnContact As System.Windows.Forms.Button
+    Friend WithEvents btnGame As System.Windows.Forms.Button
+    Friend WithEvents btnInfo As System.Windows.Forms.Button
+    Friend WithEvents lblQuote As System.Windows.Forms.Label
+    Friend WithEvents lblInfo As System.Windows.Forms.Label
+    Friend WithEvents pbLogo1 As System.Windows.Forms.PictureBox
+    Friend WithEvents btnHome As System.Windows.Forms.Button
+    Friend WithEvents pbLogo2 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbFacebook As System.Windows.Forms.PictureBox
+    Friend WithEvents pbLinkedin As System.Windows.Forms.PictureBox
+    Friend WithEvents pbYoutube As System.Windows.Forms.PictureBox
+    Friend WithEvents pbTwitter As System.Windows.Forms.PictureBox
+    Friend WithEvents pbInstagram As System.Windows.Forms.PictureBox
+    Friend WithEvents pbBankBot1 As System.Windows.Forms.PictureBox
+    Friend WithEvents pbBankBot As System.Windows.Forms.PictureBox
+    Friend WithEvents pbSuggestion As System.Windows.Forms.PictureBox
+    Friend WithEvents pbFinances As System.Windows.Forms.PictureBox
+    Friend WithEvents pbGame As System.Windows.Forms.PictureBox
+
+End Class
---
+++ NGI Hackathon/NGI Hackathon/Form1.vb
@@ -0,0 +1,135 @@
+﻿Public Class Form1
+
+    Dim home, info, game, contact As Boolean
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        home = True
+        info = False
+        game = False
+        contact = False
+
+        pbFinances.Visible = True
+
+        pbBankBot.Visible = False
+        pbBankBot1.Visible = False
+
+        pbGame.Visible = False
+
+        pbFacebook.Visible = False
+        pbInstagram.Visible = False
+        pbTwitter.Visible = False
+        pbYoutube.Visible = False
+        pbLinkedin.Visible = False
+        pbSuggestion.Visible = False
+
+        If home = True Then
+            lblInfo.Text = "As university students ourselves, we have recognized that financial literacy is often an overlooked skill for many young adults. We are faced with a number of money management decisions and are hoping to make learning about finances more time efficient, fun and easy for youth!"
+        ElseIf info = True Then
+            lblInfo.Text = "Welcome to our Banking Helper Bot! We know that university students are very busy and can’t always make it to their nearest bank branch to get all of their financial questions answered. We made this process even easier for you by answering any questions you may have about financing, budgeting or financial tools from the comfort of your own home."
+        ElseIf game = True Then
+            lblInfo.Text = "Use our interactive game, The Cost of Life, to help plan your future! The game will ask you a few multiple choice questions to help determine the cost of your schooling- but it doesn’t stop there!! Afterwards, it will help you learn about how to save your money and the various options you have with RBC."
+        ElseIf contact = True Then
+            lblInfo.Text = "Any unanswered questions?                       Contact us anytime!                                        Phone: 1 (800) 769-2511"
+        End If
+    End Sub
+
+    Private Sub btnHome_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnHome.Click
+        home = True
+        info = False
+        game = False
+        contact = False
+
+        pbFinances.Visible = True
+
+        pbBankBot.Visible = False
+        pbBankBot1.Visible = False
+
+        pbGame.Visible = False
+
+        pbFacebook.Visible = False
+        pbInstagram.Visible = False
+        pbTwitter.Visible = False
+        pbYoutube.Visible = False
+        pbLinkedin.Visible = False
+        pbSuggestion.Visible = False
+
+        lblInfo.Text = "As university students ourselves, we have recognized that financial literacy is often an overlooked skill for many young adults. We are faced with a number of money management decisions and are hoping to make learning about finances more time efficient, fun and easy for youth!"
+    End Sub
+
+    Private Sub btnInfo_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnInfo.Click
+        info = True
+        home = False
+        game = False
+        contact = False
+
+        pbFinances.Visible = False
+
+        pbBankBot.Visible = True
+        pbBankBot1.Visible = True
+
+        pbGame.Visible = False
+
+        pbFacebook.Visible = False
+        pbInstagram.Visible = False
+        pbTwitter.Visible = False
+        pbYoutube.Visible = False
+        pbLinkedin.Visible = False
+        pbSuggestion.Visible = False
+
+        lblInfo.Text = "Welcome to our Banking Helper Bot! We know that university students are very busy and can’t always make it to their nearest bank branch to get all of their financial questions answered. We made this process even easier for you by answering any questions you may have about financing, budgeting or financial tools from the comfort of your own home."
+    End Sub
+
+    Private Sub btnGame_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnGame.Click
+        game = True
+        home = False
+        info = False
+        contact = False
+
+        pbFinances.Visible = False
+
+        pbBankBot.Visible = False
+        pbBankBot1.Visible = False
+
+        pbGame.Visible = True
+
+        pbFacebook.Visible = False
+        pbInstagram.Visible = False
+        pbTwitter.Visible = False
+        pbYoutube.Visible = False
+        pbLinkedin.Visible = False
+        pbSuggestion.Visible = False
+
+        lblInfo.Text = "Use our interactive game, The Cost of Life, to help plan your future! The game will ask you a few multiple choice questions to help determine the cost of your schooling- but it doesn’t stop there!! Afterwards, it will help you learn about how to save your money and the various options you have with RBC."
+    End Sub
+
+    Private Sub btnContact_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles btnContact.Click
+        contact = True
+        home = False
+        info = False
+        game = False
+
+        pbFinances.Visible = False
+
+        pbBankBot.Visible = False
+        pbBankBot1.Visible = False
+
+        pbGame.Visible = False
+
+        pbFacebook.Visible = True
+        pbInstagram.Visible = True
+        pbTwitter.Visible = True
+        pbYoutube.Visible = True
+        pbLinkedin.Visible = True
+        pbSuggestion.Visible = True
+
+        lblInfo.Text = "Any unanswered questions?                       Contact us anytime!                                        Phone: 1 (800) 769-2511"
+    End Sub
+
+    Private Sub pbGame_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pbGame.Click
+        System.Diagnostics.Process.Start("http://woohyu.dev.fast.sheridanc.on.ca/Game/gameStart.html")
+    End Sub
+
+    Private Sub pbBankBot1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles pbBankBot1.Click
+        System.Diagnostics.Process.Start("https://web.telegram.org/#/im?p=@NGI_G12_bot")
+    End Sub
+End Class
